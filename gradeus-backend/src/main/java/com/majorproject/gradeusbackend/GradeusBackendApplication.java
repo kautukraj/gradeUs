66e6c54956157e61c55cf36a1b7d42c37271d6e6
Lovejeet Singh Parihar <lovysparihar@gmail.com>
2023-04-02 16:09:27 +0530

    First commit

---
+++ gradeus-backend/src/main/java/com/majorproject/gradeusbackend/GradeusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.majorproject.gradeusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeusBackendApplication.class, args);
+	}
+
+}
---
+++ gradeus-backend/src/test/java/com/majorproject/gradeusbackend/GradeusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majorproject.gradeusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
