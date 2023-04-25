b8334ec86d3ad6a9ffa33daaf535fda299d78d0b
kautukraj <kautuk.raj@iiitb.ac.in>
2023-04-25 22:45:13 +0530

    added Swagger support

--- gradeus-backend/src/main/java/com/majorproject/gradeusbackend/config/SecurityConfiguration.java
+++ gradeus-backend/src/main/java/com/majorproject/gradeusbackend/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**")
+                .requestMatchers("/auth/**","/v3/api-docs/**","/swagger-ui/**")
                 .permitAll()
                 .requestMatchers("/instructor/**")
                 .hasRole("INSTRUCTOR")
